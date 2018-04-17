138a4f403b608538b98e1e237f700892ea788d93
lancecrowder <thelancecrowder@gmail.com>
2018-04-17 15:28:48 -0500

    resume added

--- lanceJS.js
+++ lanceJS.js
@@ -1,7 +1,13 @@
 
 
 $('#about').click(function () {
-    console.log('i got clicked!');
+    // console.log('i got clicked!');
     // event.preventDefault();
     $('#aboutMe, img').toggleClass('invisible');
+});
+
+$('#resume').click(function () {
+    // console.log('i got clicked!');
+    // event.preventDefault();
+    $('iframe, img').toggleClass('invisible');
 });
