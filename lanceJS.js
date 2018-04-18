3c0ed6ff5a915d8cf2cf2ac200c175c0bd32c93c
lancecrowder <thelancecrowder@gmail.com>
2018-04-18 12:28:11 -0500

    need to remove each 'tab' when others are clicked!

--- lanceJS.js
+++ lanceJS.js
@@ -4,10 +4,48 @@ $('#about').click(function () {
     // console.log('i got clicked!');
     event.preventDefault();
     $('#aboutMe, img').toggleClass('invisible');
+    $('iframe:visible').toggleClass('invisible');
+    $('.contactMe:visible').toggleClass('invisible');
+    $('img:visible').toggleClass('invisible');
 });
 
 $('#resume').click(function () {
     // console.log('i got clicked!');
     event.preventDefault();
     $('iframe, img').toggleClass('invisible');
+    $('#aboutMe:visible').toggleClass('invisible');
+    $('.contactMe:visible').toggleClass('invisible');
+    $('img:visible').toggleClass('invisible');
+
+});
+
+$('#me').click(function () {
+   event.preventDefault();
+   $('iframe:visible').toggleClass('invisible');
+   $('#aboutMe:visible').toggleClass('invisible');
+    $('.contactMe:visible').toggleClass('invisible');
+    $('img').toggleClass('invisible');
+});
+
+$('#me').hover(function () {
+    $(this).css({'color': '#9E082C'});
+}, function () {
+    $(this).css({'color':'#4F4B5C'})
+});
+
+$('#contact').click(function () {
+    // console.log('i got clicked!');
+    event.preventDefault();
+    $('.contactMe, img').toggleClass('invisible');
+    $('#aboutMe:visible').toggleClass('invisible');
+    $('img:visible').toggleClass('invisible');
+    $('iframe:visible').toggleClass('invisible');
+});
+
+$('#projects').click(function () {
+    event.preventDefault();
+    $('#projectMe, img').toggleClass('invisible');
+    $('#aboutMe:visible').toggleClass('invisible');
+    $('img:visible').toggleClass('invisible');
+    $('iframe:visible').toggleClass('invisible');
 });
