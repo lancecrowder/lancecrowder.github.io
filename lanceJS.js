6bec6d321f500eccd6072735fee6b96d914591a4
lancecrowder <thelancecrowder@gmail.com>
2018-04-18 15:44:33 -0500

    need to remove each 'tab' when others are clicked! - Done!

--- lanceJS.js
+++ lanceJS.js
@@ -3,20 +3,23 @@
 $('#about').click(function () {
     // console.log('i got clicked!');
     event.preventDefault();
-    $('#aboutMe, img').toggleClass('invisible');
+    $('#aboutMe:hidden').toggleClass('invisible');
     $('iframe:visible').toggleClass('invisible');
     $('.contactMe:visible').toggleClass('invisible');
     $('img:visible').toggleClass('invisible');
+    $('#projectMe:visible').toggleClass('invisible');
+    $('#socialMediaMe:visible').toggleClass('invisible');
 });
 
 $('#resume').click(function () {
     // console.log('i got clicked!');
     event.preventDefault();
-    $('iframe, img').toggleClass('invisible');
+    $('iframe:hidden').toggleClass('invisible');
     $('#aboutMe:visible').toggleClass('invisible');
     $('.contactMe:visible').toggleClass('invisible');
     $('img:visible').toggleClass('invisible');
-
+    $('#projectMe:visible').toggleClass('invisible');
+    $('#socialMediaMe:visible').toggleClass('invisible');
 });
 
 $('#me').click(function () {
@@ -24,28 +27,45 @@ $('#me').click(function () {
    $('iframe:visible').toggleClass('invisible');
    $('#aboutMe:visible').toggleClass('invisible');
     $('.contactMe:visible').toggleClass('invisible');
-    $('img').toggleClass('invisible');
-});
-
-$('#me').hover(function () {
-    $(this).css({'color': '#9E082C'});
-}, function () {
-    $(this).css({'color':'#4F4B5C'})
+    $('img:hidden').toggleClass('invisible');
+    $('#projectMe:visible').toggleClass('invisible');
+    $('#socialMediaMe:visible').toggleClass('invisible');
 });
 
 $('#contact').click(function () {
     // console.log('i got clicked!');
     event.preventDefault();
-    $('.contactMe, img').toggleClass('invisible');
+    $('.contactMe:hidden').toggleClass('invisible');
     $('#aboutMe:visible').toggleClass('invisible');
     $('img:visible').toggleClass('invisible');
     $('iframe:visible').toggleClass('invisible');
+    $('#projectMe:visible').toggleClass('invisible');
+    $('#socialMediaMe:visible').toggleClass('invisible');
 });
 
 $('#projects').click(function () {
     event.preventDefault();
-    $('#projectMe, img').toggleClass('invisible');
+    $('#projectMe:hidden').toggleClass('invisible');
     $('#aboutMe:visible').toggleClass('invisible');
     $('img:visible').toggleClass('invisible');
     $('iframe:visible').toggleClass('invisible');
+    $('#socialMediaMe:visible').toggleClass('invisible');
+    $('.contactMe:visible').toggleClass('invisible');
+});
+
+$('#socialMedia').click(function () {
+   event.preventDefault();
+   $('#socialMediaMe:hidden').toggleClass('invisible');
+    $('#aboutMe:visible').toggleClass('invisible');
+    $('img:visible').toggleClass('invisible');
+    $('iframe:visible').toggleClass('invisible');
+    $('#projectMe:visible').toggleClass('invisible');
+
+});
+
+
+$('#me').hover(function () {
+    $(this).css({'color': '#9E082C'});
+}, function () {
+    $(this).css({'color':'#4F4B5C'});
 });
